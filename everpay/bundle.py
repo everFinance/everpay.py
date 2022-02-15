6176de8e7e5382b028691f9a65607cb703cbd986
xiaojay <xiaojay@gmail.com>
2022-02-15 16:34:27 +0800

    add load_bundle func

--- everpay/bundle.py
+++ everpay/bundle.py
@@ -1,3 +1,4 @@
+import json
 from jose.utils import base64url_encode
 from web3.auto import w3
 from eth_account.messages import encode_defunct, _hash_eip191_message
@@ -25,7 +26,8 @@ class BundleData:
     def __init__(self, item, expiration, salt, version):
         
         self.items = []
-        self.add_item(item)
+        if item:
+            self.add_item(item)
 
         self.expiration = expiration
         self.salt = salt
@@ -99,6 +101,20 @@ class BundleData:
             self.add_sig(address, sig.signature.hex())
         # print(sig.signature.hex())
 
+def load_bundle(data):
+    #{"bundle":{"items":[{"tag":"ethereum-usdt-0xdac17f958d2ee523a2206206994597c13d831ec7","chainID":"1","from":"0xFc772b419846960502089ee43CC66B7312AeFfaD","to":"0xcc7dbEd78e88e79A898d2fe0851F89BB67886a34","amount":"67287759"},{"tag":"arweave,ethereum-ar-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,0x4fadc7a98f2dc96510e42dd1a74141eeae0c1543","chainID":"0,1","from":"0xcc7dbEd78e88e79A898d2fe0851F89BB67886a34","to":"0xFc772b419846960502089ee43CC66B7312AeFfaD","amount":"2000000000000"}],"expiration":1644912733,"salt":"1bbac9f3-f097-44ea-9ef9-9c6fa85a8a11","version":"v1","sigs":{"0xFc772b419846960502089ee43CC66B7312AeFfaD":"0xc675cf6370a491a8b32a78bcb307b3b334c28772d71dadb7f31e4eb5a8280db11bb3f1e503282e4b96ee9e501ffe8c3ec62529bd0008fe84ec3629680d28001e1b","0xcc7dbEd78e88e79A898d2fe0851F89BB67886a34":"0x088a726ef0edeb882ca5ae207f2c63b6a9ab04cd9437c1118fd33a52a6d9b657613821b119c3f91038b9d80fe2cef85fef4df44b7a82017c3c594630dc8497121c"}}}
+
+    if type(data) == type('bundle'):
+        data = json.loads(data)
+    
+    data = data['bundle']
+    bundle = BundleData(None, data['expiration'], data['salt'], data['version'])
+    bundle.sigs = data['sigs']
+    for i in data['items']:
+        item = BundleItem(i['tag'], i['chainID'], i['from'], i['to'], i['amount'])
+        bundle.add_item(item)
+
+    return bundle
 
 
 

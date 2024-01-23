3048a8994c6459f23a70075b0fb036a26e39266e
xiaojay <xiaojay@gmail.com>
2024-01-23 21:12:03 +0800

    fix import bug

--- everpay/__init__.py
+++ everpay/__init__.py
@@ -2,4 +2,4 @@ from .client import Client
 from .account import Account
 from .signer import ARSigner, ETHSigner
 from .bundle import *
-from transaction import Transaction, sign_tx
+from .transaction import Transaction, sign_tx
